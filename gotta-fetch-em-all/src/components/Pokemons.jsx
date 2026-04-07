8330c59b265e4f91c91f5bb0e07094cd4a320245
Jakucs Viktor <jakucs42@gmail.com>
2026-04-07 23:07:39 +0200

    we can see pokemons in a place

--- gotta-fetch-em-all/src/components/Pokemons.jsx
+++ gotta-fetch-em-all/src/components/Pokemons.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react"
+
 function Pokemons(props){
-    console.log(props.selectedLocation)
+    
+    const [pokemonURLs, setPokemonURLs] = useState([])
+
+    let nextURL = props.selectedLocation.url
+    useEffect(()=>{
+        async function getPokemon(){
+            let res = await fetch(nextURL)
+            let data = await res.json()
+            let nextURL2 = data.areas[0].url
+            let res2 = await fetch(nextURL2)
+            let data2 = await res2.json()
+            //console.log(data2)
+            let pokemonList = data2.pokemon_encounters
+            console.log(pokemonList)
+            
+            const urls = pokemonList.map(p=>p.url)
+            setPokemonURLs(urls)
+        }
+        getPokemon()
+        console.log(pokemonURLs)
+    }, [])
+    
+    return(
+        <div key={props.selectedLocation.name}>
+            <h3>{props.selectedLocation.name}</h3>
+        </div>
+    )
 }
 
 export default Pokemons
