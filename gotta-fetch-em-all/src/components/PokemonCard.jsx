729330555b7e37bad15c82798e5801748355492e
Jakucs Viktor <jakucs42@gmail.com>
2026-04-10 14:07:25 +0200

    user can select his/her pokemon

--- gotta-fetch-em-all/src/components/PokemonCard.jsx
+++ gotta-fetch-em-all/src/components/PokemonCard.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
+
 function PokemonCard({pokemon, myPokemons}){
+
+    const [myFighterPokemon, setMyFighterPokemon] = useState({})
+
     const animatedImage =
     pokemon.sprites?.other?.showdown?.front_default;
     const staticImage = pokemon.sprites?.front_default;
@@ -16,26 +22,44 @@ function PokemonCard({pokemon, myPokemons}){
                 />
                 <br />
                 <br />
-                <h3>User's Pokémons: </h3>
 
-                {myPokemons.map((myPokemon) => {
-                    const animated =
-                    myPokemon.sprites?.other?.showdown?.front_default;
-                    const staticImage = myPokemon.sprites?.front_default;
-                        return (
-                            <div key={myPokemon.name}>
-                                <h3>{myPokemon.name}</h3>
+            {(myFighterPokemon.name) ? (
+                        <div key={myFighterPokemon.name}>
+                            <h3>{myFighterPokemon.name}</h3>
                             <img
-                        src={animated || staticImage}
-                        alt={myPokemon.name}
+                            src={myFighterPokemon.sprites?.other?.showdown?.front_default || myFighterPokemon.sprites?.front_default}
+                            alt={myFighterPokemon.name}
                     />
-                            </div>
-                        )
-                    
-                })}
+                        </div>
+            ): (
+                <div>
+                    <h3>User's Pokémons: </h3>
+                    {myPokemons.map((myPokemon) => {
+                        const animated =
+                        myPokemon.sprites?.other?.showdown?.front_default;
+                        const staticImage = myPokemon.sprites?.front_default;
+                            return (
+                                <div key={myPokemon.name}>
+                                    <h3>{myPokemon.name}</h3>
+                                <img
+                            src={animated || staticImage}
+                            alt={myPokemon.name}
+                            onClick={()=>setMyFighterPokemon(myPokemon)}
+                        />
+                                </div>
+                            )
+                        
+                    })}
+                </div>
+            )}
+
+
+
+                    {console.log(myFighterPokemon)}
             </div>
         </div>
     )
+
 }
 
 export default PokemonCard
